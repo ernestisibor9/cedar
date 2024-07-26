2925216cf40e9ddac3bd9f282e738031fb1d9cd2
Ernest <ernestisibor9@gmail.com>
2024-07-26 03:35:09 +0100

    Home page customization

---
+++ resources/views/frontend/home/gallery-area.blade.php
@@ -0,0 +1,14 @@
+
+
+<div class="container mb-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <img src="{{asset('frontend/images/little-girl.jpg')}}" alt="" class="img-fluid">
+        </div>
+        <div class="col-md-6">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/XtyxSv0tQKI?si=DKFkiEtJ87BAO2Sl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <img src="{{asset('frontend/images/parent-involve.jpg')}}" alt="" class="img-fluid">
+            <h2 class="text-center mt-2">Parent's Involved in Coding</h2>
+        </div>
+    </div>
+</div>
---
+++ resources/views/frontend/home/project-area.blade.php
@@ -0,0 +1,23 @@
+
+
+<div class="container mb-5">
+    <div class="category-content-wrap d-flex justify-content-between">
+        <div class="section-heading">
+            <h5 class="ribbon ribbon-lg mb-2">Projects</h5>
+            <h2 class="section__title">Student's Project</h2>
+            <span class="section-divider"></span>
+        </div>
+        <div class="category-btn-box text-right">
+            <a href="categories.html" class="btn theme-btn">All Projects <i
+                    class="la la-arrow-right icon ml-1"></i></a>
+        </div><!-- end section-heading -->
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/x9ApW-MOy3g?si=qGzqBQmygNVrpjCs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-6">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/CNse0B5XlQM?si=s5obc-0BqySzt4gY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
